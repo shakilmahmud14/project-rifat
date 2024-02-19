3b588552507420531820d94dd45d66598bfbe1b6
shakilmahmud14 <forallworks1414@gmail.com>
2024-02-19 23:34:34 +0600

    more added

--- assist/js/scripts.js
+++ assist/js/scripts.js
@@ -1,15 +1,14 @@
-jQuery(document).ready(function(){
-    jQuery("ul li:has(ul)").hover(function(){
-        jQuery(this).find(" > ul").slideDown("fast");
-    },function(){
-        jQuery(this).find(" > ul").slideUp("slow");
-    }
-    )
-    // upper menu js hare
+jQuery(document).ready(function() {
+    jQuery("ul li:has(ul)").hover(function() {
+            jQuery(this).find(" > ul").slideDown("fast");
+        }, function() {
+            jQuery(this).find(" > ul").slideUp("slow");
+        })
+        // upper menu js hare
 
-    jQuery(window).scroll(function(){
+    jQuery(window).scroll(function() {
         var scrolltop = jQuery(window).scrollTop();
-        if (scrolltop >= 240){
+        if (scrolltop >= 240) {
             jQuery(".upper-menu").slideDown("slow");
         } else {
             jQuery(".upper-menu").slideUp("slow");
@@ -23,83 +22,147 @@ jQuery(document).ready(function(){
 
 
     jQuery('.stellarnav').stellarNav({
-                theme: 'dark',
-                breakpoint: 960,
-                position: 'right',
-                phoneBtn: '18009997788',
-                locationBtn: 'https://www.google.com/maps'
-            });
+
+        theme: 'dark',
+        breakpoint: 960,
+        position: 'right',
+        phoneBtn: '18009997788',
+        locationBtn: 'https://www.google.com/maps'
+
+    });
 
 
-    jQuery(".feature-block-one").hover(function(){
+    jQuery(".feature-block-one").hover(function() {
         jQuery(".block-one-overly").addClass(".ovely");
     })
 
 
     // work carousel js
     $(".banner-carousel").owlCarousel({
-        margin:0,
-        nav:true,
-        dots:false,
+        margin: 0,
+        nav: true,
+        dots: false,
         items: 1,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         active: true,
         smartSpeed: 1000,
         autoplay: 6000,
-        navText: [ '<span class="fa-solid fa-arrow-left-long"></span>', '<span class="fa-solid fa-arrow-right-long"></span>' ],
-       responsive:{
-                0:{
-                    items:1,
-                    nav:false,
-                },
-                600:{
-                    items:1
-                },
-                800:{
-                    items:1
-                },
-                1024:{
-                    items:1
-                }
+        navText: ['<span class="fa-solid fa-arrow-left-long"></span>', '<span class="fa-solid fa-arrow-right-long"></span>'],
+        responsive: {
+            0: {
+                items: 1,
+                nav: false,
+            },
+            600: {
+                items: 1
+            },
+            800: {
+                items: 1
+            },
+            1024: {
+                items: 1
             }
+        }
     });
     $(".service-carousel").owlCarousel({
-        margin:0,
-        nav:true,
-        dots:false,
+        margin: 0,
+        nav: true,
+        dots: false,
         loop: true,
         items: 3,
         margin: 30,
         active: true,
         smartSpeed: 1000,
         autoplay: 6000,
-        navText: [ '<span class="fa-solid fa-arrow-left-long"></span>', '<span class="fa-solid fa-arrow-right-long"></span>' ],
-       responsive:{
-                0:{
-                    items:1,
-                },
-                600:{
-                    items:1
-                },
-                800:{
-                    items:3
-                },
-                1024:{
-                    items:3
-                }
+        navText: ['<span class="fa-solid fa-arrow-left-long"></span>', '<span class="fa-solid fa-arrow-right-long"></span>'],
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1
+            },
+            800: {
+                items: 3
+            },
+            1024: {
+                items: 3
             }
+        }
     });
 
 
+    // counter js start hare
 
+    var a = 0;
+    $(window).scroll(function() {
+        var oTop = $("#counter-box").offset().top - window.innerHeight;
+        if (a == 0 && $(window).scrollTop() > oTop) {
+            $(".counter").each(function() {
+                var $this = $(this),
+                    countTo = $this.attr("data-number");
+                $({
+                    countNum: $this.text()
+                }).animate({
+                        countNum: countTo
+                    },
 
+                    {
+                        duration: 1000,
+                        easing: "swing",
+                        step: function() {
+                            //$this.text(Math.ceil(this.countNum));
+                            $this.text(
+                                Math.ceil(this.countNum).toLocaleString("en")
+                            );
+                        },
+                        complete: function() {
+                            $this.text(
+                                Math.ceil(this.countNum).toLocaleString("en")
+                            );
+                            //alert('finished');
+                        }
+                    }
+                );
+            });
+            a = 1;
+        }
+    });
+    // counter js end hare
+
+    $(".testimonial-carousel").owlCarousel({
+        nav: false,
+        dots: false,
+        loop: true,
+        items: 3,
+        margin: 30,
+        active: true,
+        smartSpeed: 1000,
+        autoplay: 6000,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1
+            },
+            800: {
+                items: 3
+            },
+            1024: {
+                items: 3
+            }
+        }
+    });
+
+    $('select').stbDropdown();
 
 
 
     // top button js start hare
-    jQuery(".top-btn").click(function(){
-        jQuery("html").animate({scrollTop : "0px"}, "fast");
+    jQuery(".home-btn").click(function() {
+       jQuery("html").animate({scrolltop: "0px"}, "fast")
     });
 
 
